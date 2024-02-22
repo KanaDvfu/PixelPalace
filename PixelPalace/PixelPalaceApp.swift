2b6242d03df7b22bfb4fa22f78a98d3180481313
CMKMBA <ekanashkin2003@gmail.com>
2024-02-22 19:15:54 +1000

    Initial Commit

---
+++ PixelPalace/PixelPalaceApp.swift
@@ -0,0 +1,32 @@
+//
+//  PixelPalaceApp.swift
+//  PixelPalace
+//
+//  Created by Евгений Канашкин on 22.02.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PixelPalaceApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
