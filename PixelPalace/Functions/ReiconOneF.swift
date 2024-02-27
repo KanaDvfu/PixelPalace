5f05143f79e15b88aec411b944bf86f117bf0337
CMKMBA <CMKMBA>
2024-02-27 23:17:42 +1000

    Totally destroyed a bug on changing icon...

--- PixelPalace/Functions/ReiconOneF.swift
+++ PixelPalace/Functions/ReiconOneF.swift
@@ -5,13 +5,10 @@
 //  Created by Евгений Канашкин on 25.02.2024.
 //
 
-import SwiftUI
 import SwiftData
 
 /// Sets [item] icon to [icon]
 func reiconOneF(modelContext: ModelContext, item: Item, icon: String) {
-    withAnimation {
-        item.icon = icon
-        try? modelContext.save()
-    }
+    item.icon = icon
+    try? modelContext.save()
 }
